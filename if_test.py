03b461abd9a97077bcc7cb4c5dc7ae442b1bf517
Akinjokun Oluwatobi <125365846+moscarce@users.noreply.github.com>
2023-06-06 15:50:12 +0100

    if test

---
+++ if_test.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+if age < 18:
+	print('Your age is', age, 'and you are too young to participate')
+else:
+	print('Wow you are', age, '\nThat means you are good to go')
